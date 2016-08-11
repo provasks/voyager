ea4f712764ad187dba43bc4b0093a826bacff9b9
Provas Kumar Saha <provasks@gmail.com>
2016-08-11 22:42:59 +0530

    Uploaded Voyager.Common along with Main project

---
+++ Voyager.Common/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voyager.Common
+{
+    public class Logger
+    {
+        static string dirError = "AppErrors";
+        public static void LogException(string className, System.Reflection.MethodBase methodBase, Exception ex)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            DateTime now = DateTime.Now;
+            File.WriteAllText(
+                String.Format("{0}/{1}/{2}-{3}.txt", path, dirError, className, now.ToString("ddMMyyyy")),
+                String.Format("\nAt {0}\nIn the Method: {1}\nError Message: {2}\nStackTrace: {3}.txt",
+                now.ToString("hh:mm:ss"), methodBase.ToString(), ex.Message, ex.StackTrace));
+        }
+
+        public static void LogMessage(string className, System.Reflection.MethodBase methodBase, string message)
+        {
+            string appPath = Path.GetDirectoryName("AppErrors");
+            File.WriteAllText(String.Format("{0}/{1}.txt", appPath, className), message);
+        }
+    }
+}
---
+++ Voyager.Common/Utility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voyager.Common
+{
+    public class Utility
+    {
+        public string GetConfigValue(string key)
+        {
+            try
+            {
+                return ConfigurationManager.AppSettings["key"].ToString();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod(), ex);
+               // Logger.LogMessage(this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod(), "HEllo");
+                return null;
+            }
+        }
+    }
+}
